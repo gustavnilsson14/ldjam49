31de16613d9a5d3c78eb96e4f189463f29822b05
Daniel <krexor@gmail.com>
2021-10-02 23:44:59 +0200

    beetle model

--- Assets/Scripts/Game/Firefly.cs
+++ Assets/Scripts/Game/Firefly.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Firefly : BehaviourBase, IDollyMover
+public class Firefly : BehaviourBase, IDollyMover, IMelee
 {
     public CinemachineDollyCart dollyCart { get; set; }
     public CinemachineSmoothPath dollyPath { get; set; }
     public Animator animator { get; set; }
     public Vector3 previousForward { get; set; }
+    public MeleeEvent onMelee { get; set; }
 }
